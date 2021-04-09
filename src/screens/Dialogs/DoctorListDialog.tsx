96236c066a6f0e9c1196be16d06746c8657dd284
Giovana Carvalho Bombardelli <giovanabombardelli@hotmail.com>
2021-04-08 21:01:33 -0300

    render doctor list

--- src/screens/Dialogs/DoctorListDialog.tsx
+++ src/screens/Dialogs/DoctorListDialog.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Dispatch, useEffect, useState } from 'react';
 import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
@@ -12,6 +12,11 @@ import Button from '@material-ui/core/Button';
 import DialogContent from '@material-ui/core/DialogContent';
 import DialogTitle from '@material-ui/core/DialogTitle';
 import DialogActions from '@material-ui/core/DialogActions';
+import { Doctor } from '../../integration/BackendInterfaces';
+import { connect } from 'react-redux';
+import * as doctorActions from '../../store/doctor/doctorActions';
+import { Store } from '../../store/types';
+
 
 const useStyles = makeStyles((theme: Theme) =>
 	createStyles({
@@ -32,6 +37,7 @@ export default function DoctorListDialog(props: DoctorListDialogProps) {
 	const { openDialog, onClose } = props;
 	const classes = useStyles();
 	const [checked, setChecked] = React.useState([1]);
+	const [currentDoctors, setCurrentDoctors] = useState([]);
 	const handleToggle = (value: number) => () => {
 		const currentIndex = checked.indexOf(value);
 		const newChecked = [...checked];
@@ -45,27 +51,48 @@ export default function DoctorListDialog(props: DoctorListDialogProps) {
 		setChecked(newChecked);
 	};
 
+	async function getDoctors() {
+		const response = await fetch(`/doctor/all`, {
+			method: "GET",
+			headers: {
+				"Content-Type": "application/json",
+				"Access-Control-Allow-Origin": 'http://127.0.0.1:3000',
+				"Access-Control-Allow-Methods": 'GET',
+				"Access-Control-Allow-Headers": 'Content-Type, Authorization'
+			}
+		});
+
+		const data = await response.json();
+		let aux: any = [];
+		data.forEach((doctor: Doctor) => {
+			aux.push(doctor);
+		})
+		setCurrentDoctors(aux);
+	}
+
+	getDoctors();
+
 	return (
 		<Dialog open={openDialog} onClose={onClose} fullWidth>
 			<DialogTitle id="form-dialog-title">Vincular médicos</DialogTitle>
 			<DialogContent>
 				<List dense className={classes.root}>
-					{[0, 1, 2, 3].map((value: any) => {
-						const labelId = `checkbox-list-secondary-label-${value}`;
+					{currentDoctors?.map((doctor: Doctor) => {
+						const labelId = `checkbox-list-secondary-label-${doctor.doctorId}`;
 						return (
-							<ListItem key={value} button>
+							<ListItem key={doctor.doctorId} button>
 								<ListItemAvatar>
 									<Avatar
-										alt={`Avatar n°${value + 1}`}
-										src={`/static/images/avatar/${value + 1}.jpg`}
+										alt={`Avatar n°${doctor.doctorId + 1}`}
+										src={`/static/images/avatar/${doctor.doctorId + 1}.jpg`}
 									/>
 								</ListItemAvatar>
-								<ListItemText id={labelId} primary={`Line item ${value + 1}`} />
+								<ListItemText id={labelId} primary={`${doctor.doctorName}`} />
 								<ListItemSecondaryAction>
 									<Checkbox
 										edge="end"
-										onChange={handleToggle(value)}
-										checked={checked.indexOf(value) !== -1}
+										onChange={handleToggle(doctor.doctorId)}
+										checked={checked.indexOf(doctor.doctorId) !== -1}
 										inputProps={{ 'aria-labelledby': labelId }}
 									/>
 								</ListItemSecondaryAction>
